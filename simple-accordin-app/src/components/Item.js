71fc4bf0a398a15b2d373f88eb5df50d95324674
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-05-05 22:27:47 +0500

    hooks added to handle the state of the component

--- simple-accordin-app/src/components/Item.js
+++ simple-accordin-app/src/components/Item.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 
 function Item({title, text,key}) {
-    const [showAnswer, setshowAnswer] = useState(true);
+    const [showAnswer, setshowAnswer] = useState(false);
+    function handleClick(){
+      setshowAnswer(!showAnswer)
+    }
   return (
-    <div className = "item">
+    <div className = "item" onClick={handleClick}>
         <span>{key}</span>
       <h2>{title}</h2>
       {showAnswer ? <p>{text}</p>: ""}
