c2c65c48e086c1a27c11cfe3243d17a51f1e04d0
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-05-14 10:29:42 +0500

    form is controlled and state is lifted to add new friends to form

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -3,20 +3,59 @@ import FormAddFriend from "./components/FormAddFriend";
 import FriendList from "./components/FriendList";
 import FromSplitBill from "./components/FromSplitBill";
 import { useState } from "react";
-export default function App(){
-  const [showAddFriendForm, setShowAddFriendForm] = useState(false)
-function handleShowAddFriend(){
-  setShowAddFriendForm(show => !show);
-}
-  return <div className = "app">
-    <div className = "sidebar">
-      
-   <FriendList/>
-  {showAddFriendForm && <FormAddFriend/>}
-   <Button onClick={handleShowAddFriend}>{showAddFriendForm ? "Close " : "Add Friends"}</Button>
-
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+let friends = initialFriends;
+export default function App() {
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [showAddFriendForm, setShowAddFriendForm] = useState(false);
+  function handleShowAddFriend() {
+    setShowAddFriendForm((show) => !show);
+  }
+  function addFriendToList(name,imageUrl) {
+    console.log("addFriendToList is called ");
+    friends = [...friends, {id : Date.now(),name: name, image : imageUrl, balance : 0}];
+    console.log(friends);
+    setImageUrl('');
+    setName("");
+  }
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList initialFriends={friends} />
+        {showAddFriendForm && (
+          <FormAddFriend
+            name={name}
+            setName={setName}
+            imageUrl={imageUrl}
+            setImageUrl={setImageUrl}
+            addFriendToList = {addFriendToList}
+          />
+        )}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriendForm ? "Close " : "Add Friends"}
+        </Button>
+      </div>
+      <FromSplitBill />
     </div>
-      <FromSplitBill/>
-      
-  </div>
+  );
 }
--- eat-n-split/src/components/FormAddFriend.js
+++ eat-n-split/src/components/FormAddFriend.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
 
-function FormAddFriend() {
+function FormAddFriend({ name, setName, imageUrl, setImageUrl,addFriendToList }) {
+  function handleFormSubmit(e) {
+    addFriendToList(name,imageUrl)
+    e.preventDefault();
+  }
   return (
-    <form className='form-add-friend'>
-        <label htmlFor="" >👬Friend name</label>
-        <input type="text" />
-        <label htmlFor="">🏙️Image url</label>
-        <input type="text" name="" id="" />
-        
-        <Button>Add</Button>
+    <form className="form-add-friend">
+      <label htmlFor="">👬Friend name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label htmlFor="">🏙️Image url</label>
+      <input
+        type="text"
+        name=""
+        id=""
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+
+      <Button onClick={handleFormSubmit}>Add</Button>
     </form>
-  )
+  );
 }
 
-export default FormAddFriend
+export default FormAddFriend;
