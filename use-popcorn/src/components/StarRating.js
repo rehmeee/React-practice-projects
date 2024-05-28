7e0f252cc83c2cc588b3d4dc5296329cd5a0d96b
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-05-28 16:47:36 +0500

    change the code to its default state

--- use-popcorn/src/components/StarRating.js
+++ use-popcorn/src/components/StarRating.js
@@ -10,17 +10,20 @@ const styleStarBox = {
 
 }
 
-function StarRating({maxRating = 5, color  = "#fcc419", size = 48}) {
+function StarRating({maxRating = 5, size = 48, color = "#fcc149"}) {
+    const[star, setStar] = useState(0)
+    const [tempRating, setTempRating] = useState(0)
     const styleParagraph = {
-        color, 
+        // by this way user can add the color of the stars or component 
+        //color:`${color[tempRating-1]}` ,
+        color,
+      
         fontSize: `${size/1.5}px`,
         lineHeight: "1",
         margin : "0"
     
     
     }
-    const[star, setStar] = useState(0)
-    const [tempRating, setTempRating] = useState(0)
   return (
     <div style={styleOuterBox}>
         <div style={styleStarBox}>
@@ -28,7 +31,7 @@ function StarRating({maxRating = 5, color  = "#fcc419", size = 48}) {
             onHoverIn ={ ()=> setTempRating(i+1)}
             onHoverOut= { ()=> setTempRating(0)}
             full ={tempRating ? tempRating>=i +1 : star>= i + 1}
-            color= {color}
+            color= {color }
             size = {size}
            />))}
         </div>
@@ -54,8 +57,8 @@ function Star ({onClick, size, color,  onHoverIn, onHoverOut , full}){
         {full ? <svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 20 20"
-  fill={color}
-  stroke={color}
+  fill={color? color : "#fcc149"}
+  stroke={color? color : "#fcc149"}
 >
   <path
     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -65,7 +68,7 @@ function Star ({onClick, size, color,  onHoverIn, onHoverOut , full}){
   xmlns="http://www.w3.org/2000/svg"
   fill="none"
   viewBox="0 0 24 24"
-  stroke={color}
+  stroke={color? color : "#fcc149"}
 >
   <path
     strokeLinecap="round"
--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10}/>
-    {/* <StarRating maxRating={5}/>
-    <StarRating/> */}
+    <StarRating maxRating={10} color="red"/>
+    <StarRating maxRating={5} size={25}/>
+    
   </React.StrictMode>
 );
 
