54030aead5994f98ce17b29ae242f060a5f75266
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-06-04 22:49:06 +0500

    text expender modified

--- text-expander/src/components/TextExpander.js
+++ text-expander/src/components/TextExpander.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { useState } from "react";
 function TextExpander({children,showText,byDefaultShow, hideText, wordsNumCollaps}) {
     const [show, setShow] = useState(byDefaultShow)
-    let words = children.split( " ")
     
-    console.log(children);
   return (
     <div>
         <p>{children.split(" ").map((word, i)=> i<5 ? `${word} ` : `...` )}</p>
